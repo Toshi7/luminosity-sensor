863643aa432a9f4ec8b4926bb2d69413c439e882
Toshi7 <ttoshi0719@gmail.com>
2018-12-09 13:14:06 +0900

    add esp32

---
+++ esp32/test-pfi/src/main.ino
@@ -0,0 +1,17 @@
+int led= 2;
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("hello platformio!");
+    pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+    Serial.println("hello");
+    delay(100);
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(100);
+}
